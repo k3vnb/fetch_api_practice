032f7d2d9273177117300bdf9fd4e24405e60ded
Kevin Boyle <papershack@gmail.com>
2019-04-22 18:25:19 -0700

    add content

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 'use strict'
+
+function renderDogImages(json, breed) {
+  console.log(json);
+  const newImage = json.message;
+  $('.results').append(`<img class="dog-img" src=${newImage} alt='${breed} doggo!'>`).show();
+}
+
+function handleError(){
+    $('.results').append('<h2>Sorry, that breed is not found</h2>').show();
+}
+
+function getDogImage(breed) {
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+      .then(response =>   {
+        if (response.ok) {
+            return response.json();
+        } else {
+          throw new Error('Something went wrong');
+        }
+      })
+      .then(responseJson => renderDogImages(responseJson, breed))
+      .catch(error => {
+          console.log(error);
+          handleError();
+    })
+  }
+
+function resetField() {
+  $('.results').empty();
+  $('input[name="breed"]').val('');
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    $('.results').hide(20);
+    const breed = $("input[name='breed']").val();
+    resetField();
+    getDogImage(breed);
+  });
+}
+
+$(() => {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
